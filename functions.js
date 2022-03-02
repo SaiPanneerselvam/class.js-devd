90e07757f6bf8a8a1f5143d3a3ddec50612cc724
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-03-01 17:32:06 -0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,8 @@
+function transfer(){
+   window.location.href = "https://saicorporation.github.io/class.js/transfer";
+}
+
+function jcaptcha() {
+  alert("Click OK to verify you are not a robot.");
+  alert("Recorded on" + Date());
+}
