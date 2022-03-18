4eff2c597a5425ade29f9ecfe47883af7ee80ab0
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-03-18 15:22:27 -0400

    Create detectOStype.js

---
+++ detectOStype.js
@@ -0,0 +1,11 @@
+        var Name = "Unknown OS";
+        if (navigator.userAgent.indexOf("Win") != -1) OSType = 
+          "Windows OS";
+        if (navigator.userAgent.indexOf("Mac") != -1) OSType = 
+          "Macintosh";
+        if (navigator.userAgent.indexOf("Linux") != -1) OSType = 
+          "Linux OS";
+        if (navigator.userAgent.indexOf("Android") != -1) OSType = 
+          "Android OS";
+        if (navigator.userAgent.indexOf("like Mac") != -1) OSType = 
+          "iOS";
