4e1037e82753cf2fdebc5abf9d37501b5de3c03e
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-03-21 08:57:38 -0400

    Create update.py

---
+++ update/update.py
@@ -0,0 +1,32 @@
+import urllib2
+import time
+
+print("Please write the link for the update.")
+print("For more info, go to saicorporation.github.io/answers/3245365 .")
+link=input()
+print("Thank you. We will now update Class.js.")
+time.sleep(1)
+url = link
+
+file_name = url.split('/')[-1]
+u = urllib2.urlopen(url)
+f = open(file_name, 'wb')
+meta = u.info()
+file_size = int(meta.getheaders("Content-Length")[0])
+print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+file_size_dl = 0
+block_sz = 8192
+while True:
+    buffer = u.read(block_sz)
+    if not buffer:
+        break
+
+    file_size_dl += len(buffer)
+    f.write(buffer)
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = status + chr(8)*(len(status)+1)
+    print status,
+
+f.close()
+time.sleep(3)
