a8c47b8f36295e0fc2a692a92f52bf8323171b65
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-03-28 11:56:00 -0400

    Update functions.js
    - adds setimageincanvas(canvasid, image) function.
    (0())
    (()(((()()()()))))

--- functions.js
+++ functions.js
@@ -9,3 +9,10 @@ function jcaptcha() {
 function open(link4432){
   window.location.href = "link4432";
 }
+//Canvas is canvases: changeimage function
+function setimageincanvas(canvasid, image) {
+   // Sets active canvas to canvasid variable
+  setActiveCanvas(canvasid);
+   // Changes bg-img to image variable
+  drawImageURL(image);
+}
